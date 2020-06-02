fb04cb873aa8932c1be979def13d372504c13ea0
c <celer@MacBook-Air-de-Itzamara.local>
2020-06-02 02:56:45 -0700

    init with middleware. sample clean project

---
+++ fuente0.api/Controllers/Fuente0Controller.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fuente0.api.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class Fuente0Controller:ControllerBase
+    {
+        public Fuente0Controller()
+        {
+        }
+        [HttpGet("/")]
+        public IActionResult Public()
+        {
+            return Ok(new
+            {
+                Message = "This is the fuente0.com api. Be good."
+            });
+        }
+
+        [HttpGet("private")]
+        [Authorize]
+        public IActionResult PrivateTest()
+        {
+            
+            return Ok(new
+            {
+                Message = "Hello from a private endpoint! You need to be authenticated to see this."
+            });
+        }
+
+        [HttpGet("private-scoped")]
+        [Authorize("read:messages")]
+        public IActionResult ScopedTest()
+        {
+            return Ok(new
+            {
+                Message = "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this."
+            });
+        }
+
+        // This is a helper action. It allows you to easily view all the claims of the token.
+        [HttpGet("claims")]
+        public IActionResult Claims()
+        {
+            return Ok(User.Claims.Select(c =>
+                new
+                {
+                    c.Type,
+                    c.Value
+                }));
+        }
+    }
+}
---
+++ fuente0.api/Middleware/HasScopeRequirement.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using Microsoft.AspNetCore.Authorization;
+
+namespace fuente0.Middleware
+{
+    /// <summary>
+    /// This requirement checks if the scope claim issued by your Auth0 tenant is present.
+    /// If the scope claim exists, the requirement checks if the scope claim contains the requested scope.
+    /// Source: https://auth0.com/docs/quickstart/backend/aspnet-core-webapi#configure-the-middleware
+    /// </summary>
+    public class HasScopeRequirement : IAuthorizationRequirement
+    {
+        public string Issuer { get; }
+        public string Scope { get; }
+
+        public HasScopeRequirement(string scope, string issuer)
+        {
+            Scope = scope ?? throw new ArgumentNullException(nameof(scope));
+            Issuer = issuer ?? throw new ArgumentNullException(nameof(issuer));
+        }
+    }
+}
